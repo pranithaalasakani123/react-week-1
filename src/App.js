e968e0ec6e300df491a8dcdad9a8a5f70846ebe5
pranithaalasakani530@gmail.com <pranithaalasakani530@gmail.com>
2021-07-23 15:32:26 +0530

    components part-2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-
+import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
+import { DISHES } from './shared/dishes';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div classname="app">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">
-            Ristorante Con Fusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  )
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div classname="app">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              Ristorante Con Fusion
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    )
+  }
 }
+
 
 export default App;
